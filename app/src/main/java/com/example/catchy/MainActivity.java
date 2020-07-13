d3bb3bb39654be76d848d9961f52298fd22410bc
jacqueline-he <jacquelinehe00@gmail.com>
2020-07-13 15:42:17 -0700

    Create bottom navigation view and associated fragments

--- app/src/main/java/com/example/catchy/MainActivity.java
+++ app/src/main/java/com/example/catchy/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.catchy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.catchy.fragments.HomeFragment;
+import com.example.catchy.fragments.SearchFragment;
+import com.example.catchy.fragments.UserFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new HomeFragment();
+                        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                        break;
+                    case R.id.action_search:
+                        fragment = new SearchFragment();
+                        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                        break;
+                    case R.id.action_user:
+                    default:
+                        fragment = new UserFragment();
+                        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
