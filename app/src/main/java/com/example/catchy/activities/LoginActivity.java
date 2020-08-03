7a9e0bf30beb46546b0afdaae8b30c245ea5f1e0
jacqueline-he <jacquelinehe00@gmail.com>
2020-08-03 09:52:29 -0700

    Customize login activity

--- app/src/main/java/com/example/catchy/activities/LoginActivity.java
+++ app/src/main/java/com/example/catchy/activities/LoginActivity.java
@@ -1,21 +1,28 @@
 package com.example.catchy.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.catchy.R;
 import com.example.catchy.service.SpotifyBroadcastReceiver;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
+
+import co.revely.gradient.RevelyGradient;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -24,6 +31,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etPassword;
     private Button btnLogin;
     private Button btnSignup;
+    private ImageView ivDisc;
+    private RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +49,17 @@ public class LoginActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
         btnSignup = findViewById(R.id.btnSignup);
+        ivDisc = findViewById(R.id.ivDisc);
+        layout = findViewById(R.id.layout);
+
+        Glide.with(this)
+                .load(R.drawable.blue_disc)
+                .into(ivDisc);
+
+        RevelyGradient
+                .linear()
+                .colors(new int[]{Color.parseColor("#000000"), Color.parseColor("#00EDFF")}).angle(270f).alpha(0.13f)
+                .onBackgroundOf(layout);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
